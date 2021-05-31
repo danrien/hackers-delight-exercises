facddf1f0d237c4a1d83bb2515c3fde711b10200
David Vedvick <dvedvick@gmail.com>
2021-05-31 14:20:57 -0500

    Give function better name

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,10 @@ fn main() {
     println!("I have a program to show you {}. It will given the next higher number after the passed in number that has the same number of 1-bits.", name);
 
     let x: u16 = 0b1000_0000_1111_0000;
-    println!("For example, imagine I pass in 0b{:16b}. The next higher number with the same number of 1-bits is 0b{:16b}", x, snoob(x));
+    println!("For example, imagine I pass in 0b{:16b}. The next higher number with the same number of 1-bits is 0b{:16b}", x, next_highest_one_bits(x));
 }
 
-fn snoob(x: u16) -> u16 {
+fn next_highest_one_bits(x: u16) -> u16 {
     let smallest = x & (-(x as i16) as u16);
     let ripple = x + smallest;
     let mut ones = x ^ ripple;
